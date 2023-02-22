c85f4deccd8ca8fa6b979edaa9d6a6fb3b75cea4
AlanCanalesM <alan.canales@micorreo.upp.edu.mx>
2023-02-21 22:01:47 -0600

    first commit

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,10 @@
+import NewMeetUpForm from "../components/layout/meetups/NewMeetupForm";
+
+function NewMeetUp(){
+    return <section>
+        <h1>Add new Meetup</h1>
+        <NewMeetUpForm/>
+    </section>
+}
+
+export default NewMeetUp;
