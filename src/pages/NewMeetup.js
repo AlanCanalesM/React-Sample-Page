5841d36170fcc1d8dbe915d25588df98032eb149
AlanCanalesM <alan.canales@micorreo.upp.edu.mx>
2023-02-21 23:05:47 -0600

    I added firebase and sent http request to save the data

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,29 @@
 import NewMeetUpForm from "../components/layout/meetups/NewMeetupForm";
 
+
 function NewMeetUp(){
+
+
+    function addMeetupHandler(meetupData){
+
+        fetch(
+            ' https://react-getting-started-c9e2f-default-rtdb.firebaseio.com/meetups.json'
+            , {
+                method: 'POST', 
+                body: JSON.stringify(meetupData),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+    }
+
     return <section>
         <h1>Add new Meetup</h1>
-        <NewMeetUpForm/>
+        <NewMeetUpForm onAddMeetup={addMeetupHandler}/>
     </section>
 }
+
+// https://console.firebase.google.com/project/react-getting-started-c9e2f/database/react-getting-started-c9e2f-default-rtdb/data/~2F?hl=es-419
 
 export default NewMeetUp;
